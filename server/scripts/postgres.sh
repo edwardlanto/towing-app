66e03c1d8a3e48a50725a5e4ff89c3440accef45
Edward Lanto <edwardlanto@Edwards-MacBook-Pro.local>
2022-11-13 14:13:32 -0800

    Initial comit

---
+++ server/scripts/postgres.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker run --name some-postgres -p 5432:5432 \
+    -e POSTGRES_USER=taxi -e POSTGRES_DB=taxi -e POSTGRES_PASSWORD=taxi -d postgres
---
+++ server/scripts/redis.sh
@@ -0,0 +1,1 @@
+docker run --name redis-instance -p 6379:6379 -d redis
